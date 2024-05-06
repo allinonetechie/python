7dca036556f59e8166fb229c13b0d4a026a38490
Waqas Ahmed <itc1231@gmail.com>
2024-05-06 19:18:13 -0400

    variables

---
+++ variables.py
@@ -0,0 +1,31 @@
+x = 5
+y = "John"
+print(x)
+print(y + '\n')
+
+### casting
+x = str(3)
+y = int(3)
+z = float(3)
+
+print(x)
+print(y)
+print(z)
+
+print('\n')
+
+
+### type
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+print('\n')
+
+### case sensitive
+
+a = 4
+A = "Sally"
+
+print(a)
+print(A)
