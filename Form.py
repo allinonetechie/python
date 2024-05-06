1df4d1e2fce3933af3f2126517c8a11d985e4bd8
Waqas Ahmed <itc1231@gmail.com>
2024-05-06 19:37:21 -0400

    ifelse condition

---
+++ Form.py
@@ -0,0 +1,14 @@
+FirstName = "Waqas"
+LastName = "Ahmed"
+Age = 17
+DOB = "6/17/2023"
+ 
+ 
+if Age > 18:
+    print("Welcome to our website! \n")
+else:
+    print("Visitors under age 18 are not allowed to visit out website. Please come back later when you are older than 18. \n")    
+
+
+
+print("Hello " + FirstName + " " + LastName + " I see that your date of birth is " + DOB)
