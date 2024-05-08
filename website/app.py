57e8ad9d307a719184c280a3000e594b1f273c6d
Waqas Ahmed <itc1231@gmail.com>
2024-05-07 21:37:19 -0400

    update

--- website/app.py
+++ website/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='../website')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -21,9 +21,14 @@ def index():
 
         welcome_message = f"Hello {first_name} {last_name}, I see that your date of birth is {dob}"
 
-        return render_template('result.html', welcome_message=welcome_message, message=message)
+        return render_template('registration/result.html', welcome_message=welcome_message, message=message)
 
-    return render_template('index.html')
+    return render_template('registration/index.html')
+
+@app.route('/store', methods=['GET', 'POST'])
+def store():
+    return render_template('store/index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
