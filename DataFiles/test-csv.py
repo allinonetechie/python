1a7a6b62b08f32ff122e3be4589fdda18608d5fa
Waqas Ahmed <itc1231@gmail.com>
2024-05-16 21:38:29 -0400

    matplotlib

---
+++ DataFiles/test-csv.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+df = pd.read_csv('/Users/waqasahmed/Github Repo/python/python/DataFiles/data.csv')
+
+print(df.to_string())
--- slicing/slicing.py
+++ slicing/slicing.py
@@ -2,4 +2,11 @@ import numpy as np
 arr = np.array([[['123', 'Main Street', 'Anytown', 'USA'], ['456', 'Elm Avenue', 'Springfield', 'CA']], [['789', 'Oak Drive', 'Lakeside', 'NY'],['321', 'Pine Lane', 'Mountain View', 'TX']]])
 
 
-print(arr[0:1, 1:2, 1:3])
+#print(arr[0:1, 1:2, 1:3])
+
+
+for x in arr:
+  for y in x:
+    for z in y:
+      print(z)
+    print('\n') 
